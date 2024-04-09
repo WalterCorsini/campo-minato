4809e59b4f87c5bdfebcc107fbbea8f634eda2dd
WalterCorsini <corsini.walter@gmail.com>
2024-04-09 17:17:43 +0200

    add: create random bomb

--- js/function.js
+++ js/function.js
@@ -1,3 +1,4 @@
+// desscription create cell
 function createElem(number){
     const elem = document.createElement("div");
     elem.classList.add("cell");
@@ -5,6 +6,23 @@ function createElem(number){
     return elem;
 }
 
+// description click event cell
 function checkCell(){
     console.log(this.innerHTML);
+    if(this.innerHTML.includes(bombElem)){
+        alert("bomb");
+    }
+}
+
+// create random bomb under cell
+function createBomb(max,arraylength){
+const resultArray=[];
+while(resultArray.length < arraylength){
+	const rndNum = Math.floor(Math.random() * max) + 1;
+	if(!resultArray.includes(rndNum)){
+		resultArray.push(rndNum);
+    }
+} 
+console.log(resultArray);
+return resultArray;
 }
--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const btnStart = document.getElementById("btn-start");
 const containerElem = document.querySelector(".grid-container");
+const bombElem =[];
 const cell = 100;
+const bomb = 16;
 let point;
 
 // create cell
@@ -8,7 +10,11 @@ for(let i=1; i<=100; i++){
     const currElem = containerElem.append(createElem(i));
 }
 
+// creat event listener for cell
 const divElem = document.querySelectorAll(".cell");
 for(let i=0; i<100; i++){
     divElem[i].addEventListener("click", checkCell);
 }
+
+// create bomb
+createBomb(cell,bomb);
