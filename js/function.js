065a2c4ee7830d248d8259036703cbf00197b97f
WalterCorsini <corsini.walter@gmail.com>
2024-04-09 21:54:48 +0200

    fix reset points

--- js/function.js
+++ js/function.js
@@ -1,55 +1,69 @@
-// desscription create cell
-function createElem(number){
-    const elem = document.createElement("div");
-    elem.classList.add("cell");
-    elem.innerHTML = number;
-    return elem;
+/**
+ * Description: create cell
+ * @param {number} number
+ * @returns {object}  // element HTML
+ */
+function createElem(number) {
+  const elem = document.createElement("div");
+  elem.classList.add("cell");
+  elem.innerHTML = number;
+  return elem;
 }
 
-// description click event cell
-function checkCell(){
-    console.log(this.innerHTML);
-    const currElem = parseInt(this.innerHTML);
-    
-    if(bombElem.includes(currElem)){
-        this.style.backgroundColor = "red";
-        const cellElems = document.querySelectorAll(".cell");
-        console.log(cellElems);
-        for(let i=0; i < cellElems.length; i++){
-	    const elem = cellElems[i];
-        elem.style.pointerEvents="none";
-        }
-        endGame("Looser");
-    } else{
-        this.style.pointerEvents="none";
-        this.style.backgroundColor = "blue";
-        point++;
-        const resultElem = document.getElementById("result");
-        resultElem.innerHTML = point;
-        if(point === maxPoints)
-        endGame("Winner");
-        
+// description controll Cell with bomb or Not
+function checkCell() {
+  console.log(this.innerHTML);
+  const currElem = parseInt(this.innerHTML);
+
+  if (bombElem.includes(currElem)) {
+    this.style.backgroundColor = "red";
+    const cellElems = document.querySelectorAll(".cell");
+    console.log(cellElems);
+    for (let i = 0; i < cellElems.length; i++) {
+      const elem = cellElems[i];
+      elem.style.pointerEvents = "none";
     }
+    endGame("Looser");
+  } else {
+    this.style.pointerEvents = "none";
+    this.style.backgroundColor = "blue";
+    point++;
+    const resultElem = document.getElementById("result");
+    resultElem.innerHTML = point;
+    if (point === maxPoints) endGame("Winner");
+  }
 }
 
-function endGame(text){
-    if(text === "Looser"){
-        alert("hai perso!!! hai totalizzato solamente "+ point + " punti" );
-    } else if (text === "Winner"){
-        alert("hai vinto!!!!!");
-    }
+/**
+ * Description
+ * @param {string} text
+ *
+ */
+function endGame(text) {
+  if (text === "Looser") {
+    alert(
+      "hai perso!!! hai totalizzato " + point + " punti");
+  } else if (text === "Winner") {
+    alert("hai vinto!!!!!");
+  }
 }
-
 
 // create random bomb under cell
-function createBomb(max,arraylength){
-const resultArray=[];
-while(resultArray.length < arraylength){
-	const rndNum = Math.floor(Math.random() * max) + 1;
-	if(!resultArray.includes(rndNum)){
-		resultArray.push(rndNum);
+/**
+ * Description
+ * @param {number} max  // max number random
+ * @param {number} arraylength //  max number of bomb
+ * @returns {object} // elemnt of bombArray
+ */
+function createBomb(max, arraylength) {
+  const resultArray = [];
+  while (resultArray.length < arraylength) {
+    const rndNum = Math.floor(Math.random() * max) + 1;
+    if (!resultArray.includes(rndNum)) {
+      console.log(typeof resultArray);
+      resultArray.push(rndNum);
     }
-} 
-console.log(resultArray);
-return resultArray;
+  }
+  console.log(resultArray);
+  return resultArray;
 }
--- js/script.js
+++ js/script.js
@@ -5,27 +5,26 @@ const bomb = 16;
 let maxPoints = cell - bomb;
 let point = 0;
 
+btnStart.addEventListener("click", function () {
+  const resultElem = document.getElementById("result-container");
+  const resultpoint = document.getElementById("result");
+  containerElem.classList.remove("hidden");
+  containerElem.innerHTML = "";
+  resultpoint.innerHTML = "0";
+  point = 0;
 
-btnStart.addEventListener("click",function(){
-    const resultElem = document.getElementById("result-container");
-    const resultpoint = document.getElementById("result");
-    containerElem.classList.remove("hidden");
-    containerElem.innerHTML ="";
-    resultpoint.innerHTML="0";
+  resultElem.classList.remove("hidden");
 
-    resultElem.classList.remove("hidden");
-
-    // create cell
-    for(let i=1; i<=cell; i++){
-        const currElem = containerElem.append(createElem(i));
-    }
-    // creat event listener for cell
-    const divElem = document.querySelectorAll(".cell");
-    for(let i=0; i<100; i++){
-        divElem[i].addEventListener("click", checkCell);
-    }
-    });
-
+  // create cell
+  for (let i = 1; i <= cell; i++) {
+    const currElem = containerElem.append(createElem(i));
+  }
+  // creat event listener for cell
+  const divElem = document.querySelectorAll(".cell");
+  for (let i = 0; i < 100; i++) {
+    divElem[i].addEventListener("click", checkCell);
+  }
+});
 
 // create bomb
-const bombElem = createBomb(cell,bomb);
+const bombElem = createBomb(cell, bomb);
