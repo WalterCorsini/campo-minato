6c8755ca9c1e9db67c016b534d0f44a98635d591
WalterCorsini <corsini.walter@gmail.com>
2024-04-09 23:29:36 +0200

    change value in easy difficult

--- js/script.js
+++ js/script.js
@@ -23,8 +23,8 @@ btnStart.addEventListener("click", function () {
   point = 0;                                                        // RESET POINT
 // /reset game
 if(difficult == "easy"){                                            // VALUE CELL AND BOMB BASED OF DIFFICULT CHOICE
-  cell = 3;
-  bomb = 0;
+  cell = 49;
+  bomb = 5;
 } else if(difficult == "medium"){
   cell = 81;
   bomb = 10;
